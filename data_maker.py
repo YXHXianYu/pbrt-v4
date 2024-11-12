ee6097d9634d2ff24cd0b515279a3fc988100625
YXHXianYu <2943003@qq.com>
2024-11-12 11:00:36 +0800

    update

--- data_maker.py
+++ data_maker.py
@@ -126,6 +126,56 @@ def generate_data_using_a_scene(scene_name, scene_path, sppm_radius=0.02, skip_b
         "--nthreads", str(N_THREADS),
     ])
 
+def generate_preview_using_a_scene(scene_name, scene_path):
+    spp = 4
+    timestamp = get_timestamp_filename()
+    filename_ref = f"{timestamp}_{scene_name}_bdpt_{spp}.exr"
+    execute([
+        r".\build\Debug\pbrt.exe",
+        scene_path,
+        "--my-integrator", "bdpt",
+        "--spp", str(spp),
+        "--my-max-depth", str(MAX_DEPTH),
+        "--outfile", OUTPUT_FILE_PATH + filename_ref,
+        "--nthreads", str(N_THREADS),
+    ])
+
+# generate_data_using_a_scene("book", r".\scene\pbrt-book\book-common.pbrt", sppm_radius=0.2)
+# generate_data_using_a_scene("bmw", r"..\pbrt-v4-scenes-master\bmw-m6\bmw-m6-common.pbrt", sppm_radius=0.2)
+# generate_data_using_a_scene("zeroday-frame120", r"..\pbrt-v4-scenes-master\zero-day\frame120-common.pbrt")
+
+# generate_data_using_a_scene("bathroom2", r"..\benedikt-bitterli-scenes\bathroom2\scene-v4.pbrt")
+# generate_data_using_a_scene("bedroom", r"..\benedikt-bitterli-scenes\bedroom\scene-v4.pbrt")
+# generate_data_using_a_scene("cornell-box", r"..\benedikt-bitterli-scenes\cornell-box\scene-v4.pbrt")
+# generate_data_using_a_scene("dining-room", r"..\benedikt-bitterli-scenes\dining-room\scene-v4.pbrt")
+# generate_data_using_a_scene("glass-of-water", r"..\benedikt-bitterli-scenes\glass-of-water\scene-v4.pbrt")
+# generate_data_using_a_scene("kitchen", r"..\benedikt-bitterli-scenes\kitchen\scene-v4.pbrt")
+# generate_data_using_a_scene("living-room", r"..\benedikt-bitterli-scenes\living-room\scene-v4.pbrt")
+# generate_data_using_a_scene("living-room-2", r"..\benedikt-bitterli-scenes\living-room-2\scene-v4.pbrt")
+# generate_data_using_a_scene("staircase", r"..\benedikt-bitterli-scenes\staircase\scene-v4.pbrt")
+# generate_data_using_a_scene("staircase2", r"..\benedikt-bitterli-scenes\staircase2\scene-v4.pbrt")
+# generate_data_using_a_scene("veach-ajar", r"..\benedikt-bitterli-scenes\veach-ajar\scene-v4.pbrt")
+# generate_data_using_a_scene("veach-bidir", r"..\benedikt-bitterli-scenes\veach-bidir\scene-v4.pbrt")
+
+# generate_data_using_a_scene("zeroday-frame25", r"..\pbrt-v4-scenes-master\zero-day\frame25.pbrt")
+# generate_data_using_a_scene("zeroday-frame210", r"..\pbrt-v4-scenes-master\zero-day\frame210.pbrt")
+# generate_data_using_a_scene("zeroday-frame380", r"..\pbrt-v4-scenes-master\zero-day\frame380.pbrt")
+
+# generate_data_using_a_scene("bedroom-v2", r"..\benedikt-bitterli-scenes\bedroom\scene-v4-2.pbrt")
+# generate_data_using_a_scene("bedroom-v3", r"..\benedikt-bitterli-scenes\bedroom\scene-v4-3.pbrt")
+# generate_data_using_a_scene("dining-room-v2", r"..\benedikt-bitterli-scenes\dining-room\scene-v4-2.pbrt")
+# generate_data_using_a_scene("dining-room-v3", r"..\benedikt-bitterli-scenes\dining-room\scene-v4-3.pbrt")
+# generate_data_using_a_scene("glass-of-water-v2", r"..\benedikt-bitterli-scenes\glass-of-water\scene-v4-2.pbrt")
+# generate_data_using_a_scene("glass-of-water-v3", r"..\benedikt-bitterli-scenes\glass-of-water\scene-v4-3.pbrt")
+# generate_data_using_a_scene("kitchen-v2", r"..\benedikt-bitterli-scenes\kitchen\scene-v4-2.pbrt")
+# generate_data_using_a_scene("kitchen-v3", r"..\benedikt-bitterli-scenes\kitchen\scene-v4-3.pbrt")
+# generate_data_using_a_scene("living-room-v2", r"..\benedikt-bitterli-scenes\living-room\scene-v4-2.pbrt")
+# generate_data_using_a_scene("living-room-v3", r"..\benedikt-bitterli-scenes\living-room\scene-v4-3.pbrt")
+# generate_data_using_a_scene("staircase2-v2", r"..\benedikt-bitterli-scenes\staircase2\scene-v4-2.pbrt")
+generate_data_using_a_scene("staircase2-v3", r"..\benedikt-bitterli-scenes\staircase2\scene-v4-3.pbrt")
+generate_data_using_a_scene("veach-ajar-v2", r"..\benedikt-bitterli-scenes\veach-ajar\scene-v4-2.pbrt")
+generate_data_using_a_scene("veach-ajar-v3", r"..\benedikt-bitterli-scenes\veach-ajar\scene-v4-3.pbrt")
+
 # 跳过这个场景，pt <-> bdpt/sppm 的渲染结果差异过大，是一个pt无法收敛的典型场景
 # generate_data_using_a_scene("bathroom", r".\scene\contemporary-bathroom\contemporary-bathroom-common.pbrt", sppm_radius=0.02) 
 
@@ -135,23 +185,8 @@ def generate_data_using_a_scene(scene_name, scene_path, sppm_radius=0.02, skip_b
 # 跳过这个场景，这个场景1.13G，读取太慢
 # generate_data_using_a_scene("bistro-vespa", r"..\pbrt-v4-scenes-master\bistro\bistro_vespa-common.pbrt")
 
-# generate_data_using_a_scene("book", r".\scene\pbrt-book\book-common.pbrt", sppm_radius=0.2)
-# generate_data_using_a_scene("bmw", r"..\pbrt-v4-scenes-master\bmw-m6\bmw-m6-common.pbrt", sppm_radius=0.2)
-# generate_data_using_a_scene("zeroday-frame120", r"..\pbrt-v4-scenes-master\zero-day\frame120-common.pbrt")
-
-# generate_data_using_a_scene("bathroom2", r"..\benedikt-bitterli-scenes\bathroom2\scene-v4.pbrt")
-# generate_data_using_a_scene("bedroom", r"..\benedikt-bitterli-scenes\bedroom\scene-v4.pbrt")
-# generate_data_using_a_scene("classroom", r"..\benedikt-bitterli-scenes\classroom\scene-v4.pbrt")
-# generate_data_using_a_scene("cornell-box", r"..\benedikt-bitterli-scenes\cornell-box\scene-v4.pbrt")
-# generate_data_using_a_scene("dining-room", r"..\benedikt-bitterli-scenes\dining-room\scene-v4.pbrt")
-# generate_data_using_a_scene("glass-of-water", r"..\benedikt-bitterli-scenes\glass-of-water\scene-v4.pbrt")
-generate_data_using_a_scene("kitchen", r"..\benedikt-bitterli-scenes\kitchen\scene-v4.pbrt")
-generate_data_using_a_scene("living-room", r"..\benedikt-bitterli-scenes\living-room\scene-v4.pbrt")
-generate_data_using_a_scene("living-room-2", r"..\benedikt-bitterli-scenes\living-room-2\scene-v4.pbrt")
-generate_data_using_a_scene("staircase", r"..\benedikt-bitterli-scenes\staircase\scene-v4.pbrt")
-generate_data_using_a_scene("staircase2", r"..\benedikt-bitterli-scenes\staircase2\scene-v4.pbrt")
-generate_data_using_a_scene("veach-ajar", r"..\benedikt-bitterli-scenes\veach-ajar\scene-v4.pbrt")
-generate_data_using_a_scene("veach-bidir", r"..\benedikt-bitterli-scenes\veach-bidir\scene-v4.pbrt")
-
 # 跳过这个场景，这个场景是caustic场景，pt无法正确渲染
 # generate_data_using_a_scene("water-caustic", r"..\benedikt-bitterli-scenes\water-caustic\scene-v4.pbrt")
+
+# 跳过这个场景，这个场景sppm渲染太暗
+# generate_data_using_a_scene("classroom", r"..\benedikt-bitterli-scenes\classroom\scene-v4.pbrt")
